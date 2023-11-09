551d8c8188418b2e74895a0b476d0c3847a3159d
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-11-09 16:53:59 +0000

    projects created_at column defaults to to current time

--- backend/seeds/hookmark_database.sql
+++ backend/seeds/hookmark_database.sql
@@ -6,7 +6,7 @@ CREATE TABLE projects (
     name text,
     link text,
     notes text,
-    created_at timestamp
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP
 );
 
 INSERT INTO projects (name, link, notes, created_at) VALUES ('cable knit hat', 'www.google.com', 'test note', '2023-11-09 15:35:00')
