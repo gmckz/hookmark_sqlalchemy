4afe0efc257a102fc35e5db47a69fed383f1eb38
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-11-09 16:22:51 +0000

    project model recognises identical instances as equal and represents them as strings

--- backend/lib/Project.py
+++ backend/lib/Project.py
@@ -5,3 +5,11 @@ class Project:
         self.link = link
         self.notes = notes
         self.created_at = created_at
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def __repr__(self):
+        return f"Project({self.id}, {self.name}, {self.link}, {self.notes}, {self.created_at})"
+    
+    
--- backend/tests/test_Project.py
+++ backend/tests/test_Project.py
@@ -12,3 +12,20 @@ def test_contructs():
     assert project.link == 'test link'
     assert project.notes == 'test notes'
     assert project.created_at == timestamp
+
+"""
+contents are equal
+"""
+def test_equal():
+    timestamp = datetime.now()
+    project1 = Project(1, 'beanie hat', 'test link', 'test notes', timestamp)
+    project2 = Project(1, 'beanie hat', 'test link', 'test notes', timestamp)
+    assert project1 == project2
+
+"""
+represents projects as string
+"""
+def test_return_a_string():
+    timestamp = datetime(2023, 11, 9, 12)
+    project1 = Project(1, 'beanie hat', 'test link', 'test notes', timestamp)
+    assert str(project1) == "Project(1, beanie hat, test link, test notes, 2023-11-09 12:00:00)"
