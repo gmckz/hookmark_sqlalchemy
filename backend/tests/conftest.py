4aadf9834b81849f36136b073df239913f875617
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-11-10 15:40:21 +0000

    web client pytest fixture

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -8,3 +8,8 @@ def db_connection():
     conn.connect()
     return conn
 
+@pytest.fixture
+def web_client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
