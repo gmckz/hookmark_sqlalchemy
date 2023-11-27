a6c627eed2d7dcec89c1664ffe90aeccd31d1fdc
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-11-27 17:52:41 +0000

    view all projects page styled for desktop

--- frontend/src/Projects.js
+++ frontend/src/Projects.js
@@ -14,24 +14,28 @@ function Projects() {
 				setProjects(data.projects);
 			});
 	}, []);
-	console.log("logging projects", projects);
 
 	return (
 		<>
-			<h1>All Projects</h1>
-
-			{projects.map((project) => {
-				return (
-					<div key={project.id} className="project">
-						<h2>{project.name}</h2>
-						<a
-							href={`http://localhost:3000/projects/${project.id}`}
-						>
-							View Project
-						</a>
-					</div>
-				);
-			})}
+			<div className="all-projects-container">
+				<h1>All Projects</h1>
+				<div className="cards-container">
+					{projects.map((project) => {
+						return (
+							<div key={project.id} className="project">
+								<h2>{project.name}</h2>
+								<a
+									href={`http://localhost:3000/projects/${project.id}`}
+								>
+									<button className="project-btn">
+										View Project
+									</button>
+								</a>
+							</div>
+						);
+					})}
+				</div>
+			</div>
 		</>
 	);
 }
