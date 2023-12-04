d33d6548db83321efefb40ff86ef05dc023ba65f
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-12-04 20:02:08 +0000

    post request to /projects successfully adds project to database

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from flask_cors import CORS
 from lib.database_connection import get_flask_database_connection
 from lib.project_repository import ProjectRepository
+from lib.Project import Project
 
 app = Flask(__name__)
 CORS(app)
@@ -23,6 +24,16 @@ def get_all_projects():
     repository = ProjectRepository(connection)
     projects = {"projects": repository.all()}
     return projects
+
+@app.route("/projects", methods=["POST"])
+def create_project():
+    connection = get_flask_database_connection(app)
+    repository = ProjectRepository(connection)
+    project = Project(None, request.form['name'], request.form['link'], request.form['notes'])
+    repository.create(project)
+    return "Project successfully added to database"
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- backend/tests/test_server.py
+++ backend/tests/test_server.py
@@ -8,7 +8,7 @@ def test_get_a_project(db_connection, web_client):
     db_connection.seed('seeds/hookmark_database.sql')
     response = web_client.get('/projects/1')
     assert response.status_code == 200
-    assert json.loads(response.data.decode('utf-8')) == {"id": 1, "name": "cable knit hat", "link": "www.test.com", "notes": "test note", "created_at": datetime(2023, 11, 9, 15, 35).strftime('%a, %d %b %Y %H:%M:%S GMT')}
+    assert json.loads(response.data.decode('utf-8')) == {"id": 1, "name": "cable knit hat", "link": "www.test.com", "notes": "test note"}
 
 """
 When I make a GET request to /projects
@@ -25,20 +25,28 @@ def test_get_all_projects(db_connection, web_client):
                 "name": 'cable knit hat',
                 "link": 'www.test.com',
                 "notes": 'test note',
-                "created_at": datetime(2023, 11, 9, 15, 35, 0).strftime('%a, %d %b %Y %H:%M:%S GMT')
             },
             {
                 "id": 2,
                 "name": 'jumper',
                 "link": 'www.test.com',
                 "notes": 'test note 2',
-                "created_at": datetime(2023, 11, 14, 14, 14, 14).strftime('%a, %d %b %Y %H:%M:%S GMT')
             },
             {
                 "id": 3,
                 "name": 'cardigan',
                 "link": 'www.test.com',
                 "notes": 'test note 3',
-                "created_at": datetime(2023, 11, 14, 14, 15, 0).strftime('%a, %d %b %Y %H:%M:%S GMT')
             },
         ]}
+
+"""
+When I make a POST request to /projects with a valid project
+I get a 200 response and success message
+"""
+def test_create_a_project(db_connection, web_client):
+    db_connection.seed("seeds/hookmark_database.sql")
+    data={'name': 'New project', 'link':'www.test.com', 'notes': 'i\'m creating a new project'}
+    response = web_client.post('/projects', data=data)
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == "Project successfully added to database"
