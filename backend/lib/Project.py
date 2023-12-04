abb458a7254b215f21dc8807ac963009f0e5d5d2
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-12-04 11:39:23 +0000

    refactor project model and test to handle automatic created_at in database

--- backend/lib/Project.py
+++ backend/lib/Project.py
@@ -1,16 +1,15 @@
 class Project:
-    def __init__(self, id, name, link, notes, created_at):
+    def __init__(self, id, name, link, notes):
         self.id = id
         self.name = name
         self.link = link
         self.notes = notes
-        self.created_at = created_at
 
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
     def __repr__(self):
-        return f"Project({self.id}, {self.name}, {self.link}, {self.notes}, {self.created_at})"
+        return f"Project({self.id}, {self.name}, {self.link}, {self.notes})"
     
     def __name_is_invalid(self):
         if self.name == "" or self.name == None:
--- backend/tests/test_Project.py
+++ backend/tests/test_Project.py
@@ -1,55 +1,52 @@
 from lib.Project import Project
-from datetime import datetime
 
-timestamp = datetime(2023, 11, 9, 12)
 
 """
-Constructs with id, name, link, notes and created_at
+Constructs with id, name, link, notes
 """
 def test_contructs():
-    project = Project(1, 'beanie hat', 'test link', 'test notes', timestamp)
+    project = Project(1, 'beanie hat', 'test link', 'test notes')
     assert project.id == 1
     assert project.name == 'beanie hat'
     assert project.link == 'test link'
     assert project.notes == 'test notes'
-    assert project.created_at == timestamp
 
 """
 contents are equal
 """
 def test_equal():
-    project1 = Project(1, 'beanie hat', 'test link', 'test notes', timestamp)
-    project2 = Project(1, 'beanie hat', 'test link', 'test notes', timestamp)
+    project1 = Project(1, 'beanie hat', 'test link', 'test notes')
+    project2 = Project(1, 'beanie hat', 'test link', 'test notes')
     assert project1 == project2
 
 """
 represents projects as string
 """
 def test_return_a_string():
-    project1 = Project(1, 'beanie hat', 'test link', 'test notes', timestamp)
-    assert str(project1) == "Project(1, beanie hat, test link, test notes, 2023-11-09 12:00:00)"
+    project1 = Project(1, 'beanie hat', 'test link', 'test notes')
+    assert str(project1) == "Project(1, beanie hat, test link, test notes)"
 
 """
 is_valid returns true when given projects with name, link and created_at properties
 """
 def test_is_valid_true():
-    assert Project(1, 'beanie hat', 'test link', '', timestamp).is_valid() == True
-    assert Project(1, 'beanie hat', 'test link', None, timestamp).is_valid() == True
-    assert Project(1, 'beanie hat', 'test link', 'test note', timestamp).is_valid() == True
+    assert Project(1, 'beanie hat', 'test link', '').is_valid() == True
+    assert Project(1, 'beanie hat', 'test link', None).is_valid() == True
+    assert Project(1, 'beanie hat', 'test link', 'test note').is_valid() == True
 
 """
 is_valid returns false when given projects without name or link properties
 """
 def test_is_valid_false():
-    assert Project(1, '', 'test link', 'test note', timestamp).is_valid() == False
-    assert Project(1, None, '', 'test note', timestamp).is_valid() == False
-    assert Project(1, 'name', None, 'test note', timestamp).is_valid() == False
+    assert Project(1, '', 'test link', 'test note').is_valid() == False
+    assert Project(1, None, '', 'test note').is_valid() == False
+    assert Project(1, 'name', None, 'test note').is_valid() == False
 
 """
 error_message returns error message when project is invalid
 """
 def test_generate_error_messages():
-    error_message1 = Project(1, '', 'test link', 'test note', timestamp).generate_error_message()
+    error_message1 = Project(1, '', 'test link', 'test note').generate_error_message()
     assert error_message1 == "Error: name must have a value"
-    error_message2 = Project(1, 'name', None, 'test note', timestamp).generate_error_message()
+    error_message2 = Project(1, 'name', None, 'test note').generate_error_message()
     assert error_message2 == "Error: link must have a value"
