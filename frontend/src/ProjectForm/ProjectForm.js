45ee794aebcb6abdd97020ab9b8f9ed1d8e8d3d4
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-12-05 16:15:40 +0000

    form inputs update state of name, link and notes

--- frontend/src/ProjectForm/ProjectForm.js
+++ frontend/src/ProjectForm/ProjectForm.js
@@ -1,17 +1,35 @@
 import "./ProjectForm.css";
+import { useState } from "react";
 
 function ProjectForm() {
+	const [name, setName] = useState("");
+	const [link, setLink] = useState("");
+	const [notes, setNotes] = useState("");
+
 	return (
 		<>
 			<div className="project-form-container">
 				<h1>Add a new project</h1>
 				<form className="project-form">
 					<label>Project name:</label>
-					<input type="text" required />
+					<input
+						type="text"
+						required
+						value={name}
+						onChange={(e) => setName(e.target.value)}
+					/>
 					<label>Pattern link:</label>
-					<input type="text" required />
+					<input
+						type="text"
+						required
+						value={link}
+						onChange={(e) => setLink(e.target.value)}
+					/>
 					<label>Notes:</label>
-					<textarea required></textarea>
+					<textarea
+						value={notes}
+						onChange={(e) => setNotes(e.target.value)}
+					></textarea>
 					<button>Add project</button>
 				</form>
 			</div>
