571390bc8d43c77c21c2af67dc3d2d79f1f3c972
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-12-05 16:04:48 +0000

    project form styling

--- frontend/src/ProjectForm/ProjectForm.js
+++ frontend/src/ProjectForm/ProjectForm.js
@@ -1,9 +1,11 @@
+import "./ProjectForm.css";
+
 function ProjectForm() {
 	return (
 		<>
 			<div className="project-form-container">
 				<h1>Add a new project</h1>
-				<form>
+				<form className="project-form">
 					<label>Project name:</label>
 					<input type="text" required />
 					<label>Pattern link:</label>
