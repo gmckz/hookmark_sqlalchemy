6683b5338e5d1ea77c0ce6df221361dac0442cad
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-12-08 16:36:48 +0000

    navbar contains links to all projects and add project, navbar styled

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -3,14 +3,20 @@ import { Routes, Route } from "react-router-dom";
 import Project from "../Project/Project";
 import Projects from "../Projects/Projects";
 import AddProject from "../AddProject/AddProject";
+import NavBar from "../NavBar/NavBar";
 
 function App() {
 	return (
-		<Routes>
-			<Route path="/projects/:id" element={<Project />} />
-			<Route path="/projects" element={<Projects />} />
-			<Route path="/add-project" element={<AddProject />} />
-		</Routes>
+		<>
+			<NavBar />
+			<div className="app">
+				<Routes>
+					<Route path="/projects/:id" element={<Project />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/add-project" element={<AddProject />} />
+				</Routes>
+			</div>
+		</>
 	);
 }
 
