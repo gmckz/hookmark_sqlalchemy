50627614387b4c6021dd6193dd1296279d7b293a
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2023-12-11 16:27:28 +0000

    update method raises InvalidProjectException when called with an invalid project

--- backend/lib/project_repository.py
+++ backend/lib/project_repository.py
@@ -50,7 +50,8 @@ class ProjectRepository:
                         [project.name, project.link, project.notes, project.id]
                 )
             else:
-                return project.generate_error_message()
+                error_message = project.generate_error_message()
+                raise InvalidProjectException(error_message)
         except DatabaseQueryException as e:
             raise DatabaseQueryException('Error executing database query in update method') from e
         
--- backend/tests/test_project_repository.py
+++ backend/tests/test_project_repository.py
@@ -111,12 +111,38 @@ def test_update_project(db_connection):
     assert repository.find(4) == project2
 
 """
+Calling ProjectRepository.update() with an invalid project object
+returns an error message and the project is not updated
+"""
+def test_cannot_create_invalid_project(db_connection):
+    db_connection.seed("seeds/hookmark_database.sql")
+    repository = ProjectRepository(db_connection)
+    project1 = Project(4, "", None, "this project is invalid")
+    with pytest.raises(InvalidProjectException) as e:
+        repository.update(project1)
+    error_message1 = str(e.value)
+    assert error_message1 == "Error: name and link must have a value"
+
+    project2 = Project(4, "", "www.test.com", "this project is invalid")
+    with pytest.raises(InvalidProjectException) as e:
+        repository.update(project2)
+    error_message2 = str(e.value)
+    assert error_message2 == "Error: name must have a value"
+    
+    project3 = Project(4, "name", None, "this project is invalid")
+    with pytest.raises(InvalidProjectException) as e:
+        repository.update(project3)
+    error_message3 = str(e.value)
+    assert error_message3 == "Error: link must have a value"
+
+"""
 Calling ProjectRepository.delete() with a project id
 deletes the project from the database
 """
 def test_delete_project(db_connection):
     db_connection.seed("seeds/hookmark_database.sql")
     repository = ProjectRepository(db_connection)
+    print(repository.all())
     response = repository.delete(1)
     assert response == "Project with id 1 deleted"
     assert repository.find(1) == "Project with id 1 does not exist"
