7fbc774773595ad20de1a9cee398b6b1837bf336
Gemma McKenzie <gemma_mckenzie@hotmail.co.uk>
2024-03-20 14:36:10 +0000

    Defined project model class

--- backend/server.py
+++ backend/server.py
@@ -12,3 +12,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'ho
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    link = db.Column(db.String(100), nullable=True)
+    notes = db.Column(db.Text)
+
+    def __repr__(self):
+        return f"<Project {self.id} {self.name} {self.link}"
